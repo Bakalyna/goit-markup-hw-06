e8fd5878c1a05114f42be21d6335b3bbe9b2a440
Bakalyna <142011895+Bakalyna@users.noreply.github.com>
2023-10-27 03:19:52 +0300

    hw6 one

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+// Modal Window 
+
+document.getElementById('is-open-modal').addEventListener('click', function () {
+  document.getElementById('modal-is-open').classList.add('is-open');
+});
+
+document
+  .getElementById('close-modal-window')
+  .addEventListener('click', function () {
+    document.getElementById('modal-is-open').classList.remove('is-open');
+  });
+
+// Burger Menu 
+
+document
+  .getElementById('is-open-burger')
+  .addEventListener('click', function () {
+    document.getElementById('burger-is-open').classList.add('is-open');
+  });
+
+document
+  .getElementById('close-burger-menu')
+  .addEventListener('click', function () {
+    document.getElementById('burger-is-open').classList.remove('is-open');
+  });
